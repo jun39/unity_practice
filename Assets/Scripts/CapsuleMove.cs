5fa8838922ff6115bfc37473e200ecd78a0b35bc
jun39 <bbbbb12312339@gmail.com>
2022-05-06 17:39:27 +0900

    added Move

--- Assets/Scripts/CapsuleMove.cs
+++ Assets/Scripts/CapsuleMove.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class CapsuleMove : MonoBehaviour
 {
     public float speed = 3.0F;
+    // public float speed = 0.01F;
+
     public float rotateSpeed = 3.0F;
     void Update()
     {
         CharacterController controller = GetComponent<CharacterController>();
         // 左右キー Yを軸として回転
-        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
- 
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed,0);
         // 上下キー 前後移動
         Vector3 forward = transform.TransformDirection(Vector3.forward);
+        // オブジェクトの向きの座標（transform forward）をローカル座標からワールド座標に変換する
         float curSpeed = speed * Input.GetAxis("Vertical");
-        controller.SimpleMove(forward * curSpeed);
+        // controller.SimpleMove(forward * curSpeed);
+        // Moveの移動量は絶対値、入力した分だけ進む
+        controller.Move(forward * curSpeed);
+
     }
 }
