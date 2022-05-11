a6e3c059676b61f5ba983624e5abf22aea341267
jun39 <bbbbb12312339@gmail.com>
2022-05-11 23:06:50 +0900

    added GizmosExample

---
+++ Assets/Scripts/GizmosExample.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+public class GizmosExample : MonoBehaviour
+{
+    void OnDrawGizmosSelected()
+    {
+        // Draw a yellow cube at the transform position
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, new Vector3(10, 10, 10));
+    }
+}
