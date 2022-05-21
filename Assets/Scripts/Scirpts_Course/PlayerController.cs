8d7691262183cfa757d206b0675816cb45e12e0a
jun39 <bbbbb12312339@gmail.com>
2022-05-21 15:11:27 +0900

    added speed variable in P;ayerContorller.cs

--- Assets/Scripts/Scirpts_Course/PlayerController.cs
+++ Assets/Scripts/Scirpts_Course/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +14,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
         
     }
 }
