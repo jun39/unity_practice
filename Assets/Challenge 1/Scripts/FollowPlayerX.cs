8e3a4822d3cde7f24fe5afe6ccff2595ba461ddd
jun39 <bbbbb12312339@gmail.com>
2022-05-22 15:57:17 +0900

    fixed PlayerControllerX.cs

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    public Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        offset.x = 20;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = plane.transform.position + offset;
+        transform.LookAt(plane.transform);
+
     }
 }
